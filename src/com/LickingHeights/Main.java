025a801225f26a569c0c2c9962a8ef1d52a3dd7a
Emmanuel <emmanuel.ennin.2022@student.lhschools.org>
2020-03-04 13:11:38 -0500

    intelij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,14 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("My name is ");
+        System.out.println("Emmanuel");
+        System.out.println("Over the summer I went to the movies.");
+        System.out.println("I also took a trip to Canada to attend a wedding.");
+        System.out.println("To end my summer break I went to a picnic.");
+    }
+
+}
